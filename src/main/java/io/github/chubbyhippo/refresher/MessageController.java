24a38b62deb6dd25b5846bdafe5fc939767b838e
ch <mkmsacc@outlook.com>
2025-03-06 13:52:33 +0700

    Add MessageService dependency to MessageController
    Introduced MessageService to decouple message retrieval logic from MessageController. Updated the controller test to mock the service and verify message retrieval behavior, ensuring better testability and alignment with dependency injection principles.

--- src/main/java/io/github/chubbyhippo/refresher/MessageController.java
+++ src/main/java/io/github/chubbyhippo/refresher/MessageController.java
@@ -6,9 +6,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MessageController {
 
+    private final MessageService messageService;
+
+    public MessageController(MessageService messageService) {
+        this.messageService = messageService;
+    }
+
     @GetMapping("/message")
     public String getMessage() {
-        return "Default message";
+        return messageService.getMessage();
     }
 
 }
--- src/test/java/io/github/chubbyhippo/refresher/MessageControllerTest.java
+++ src/test/java/io/github/chubbyhippo/refresher/MessageControllerTest.java
@@ -2,19 +2,29 @@ package io.github.chubbyhippo.refresher;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.assertj.MockMvcTester;
+
+import static org.mockito.Mockito.*;
 
 @WebMvcTest(MessageController.class)
 class MessageControllerTest {
 
     @Autowired
     private MockMvcTester mockMvcTester;
+    @MockitoBean
+    private MessageService messageService;
 
     @Test
     @DisplayName("should return message")
     void shouldReturnMessage() {
+        when(messageService.getMessage())
+                .thenReturn("Default message");
+
         mockMvcTester.get()
                 .uri("/message")
                 .assertThat()

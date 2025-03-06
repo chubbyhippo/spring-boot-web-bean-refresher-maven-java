280fe0359bfb5187ad280984173f670f6d93397b
ch <mkmsacc@outlook.com>
2025-03-06 14:09:57 +0700

    Add test method ordering with @TestMethodOrder annotation
    Introduce @TestMethodOrder to specify execution order for tests. Annotated the existing test with @Order to ensure deterministic test runs. Minor code formatting adjustments were also made for consistency.

--- src/test/java/io/github/chubbyhippo/refresher/RefresherApplicationTests.java
+++ src/test/java/io/github/chubbyhippo/refresher/RefresherApplicationTests.java
@@ -1,26 +1,27 @@
 package io.github.chubbyhippo.refresher;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.assertj.MockMvcTester;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT )
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class RefresherApplicationTests {
 
-	@Autowired
-	private MockMvcTester mockMvcTester;
+    @Autowired
+    private MockMvcTester mockMvcTester;
 
-	@Test
-	@DisplayName("should return message")
-	void shouldReturnMessage() {
+    @Test
+    @DisplayName("should return message")
+    @Order(1)
+    void shouldReturnMessage() {
 
-		mockMvcTester.get()
-				.uri("/message")
-				.assertThat()
-				.hasBodyTextEqualTo("Default message");
-	}
+        mockMvcTester.get()
+                .uri("/message")
+                .assertThat()
+                .hasBodyTextEqualTo("Default message");
+    }
 }

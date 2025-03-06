140ed958ca8e7fcc34d453374603a06c906c570d
ch <mkmsacc@outlook.com>
2025-03-06 14:04:20 +0700

    Add integration test for `/message` endpoint
    Updated `RefresherApplicationTests` to verify the `/message` endpoint functionality. Introduced MockMvcTester for mock MVC testing and added relevant annotations for test configuration.

--- src/test/java/io/github/chubbyhippo/refresher/RefresherApplicationTests.java
+++ src/test/java/io/github/chubbyhippo/refresher/RefresherApplicationTests.java
@@ -1,13 +1,26 @@
 package io.github.chubbyhippo.refresher;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.assertj.MockMvcTester;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT )
+@AutoConfigureMockMvc
 class RefresherApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@Autowired
+	private MockMvcTester mockMvcTester;
 
+	@Test
+	@DisplayName("should return message")
+	void shouldReturnMessage() {
+
+		mockMvcTester.get()
+				.uri("/message")
+				.assertThat()
+				.hasBodyTextEqualTo("Default message");
+	}
 }

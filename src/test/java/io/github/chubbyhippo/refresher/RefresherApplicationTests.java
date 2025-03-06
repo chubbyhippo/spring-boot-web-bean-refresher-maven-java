8832e08d851696e099c173992f0ab4dd690167b9
ch <mkmsacc@outlook.com>
2025-03-06 13:10:27 +0700

    Add initial Spring Boot application setup
    This commit includes the basic structure for a Spring Boot project. It adds Maven Wrapper files, a POM file with essential dependencies, a `.gitignore` file, and a main application class along with a placeholder test. This sets up the foundation for the "Refresher" Spring Boot project.

---
+++ src/test/java/io/github/chubbyhippo/refresher/RefresherApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.chubbyhippo.refresher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RefresherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

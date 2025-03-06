7d0ff8c462ca86873be2b05d96816a2fad0e83fc
ch <mkmsacc@outlook.com>
2025-03-06 13:38:56 +0700

    Add test for changed message and enforce test order
    Introduced a new test to verify the message change functionality. Enforced test execution order using `@TestMethodOrder` and added `@Order` annotations to ensure consistent results.

--- src/test/java/io/github/chubbyhippo/refresher/MessageServiceTest.java
+++ src/test/java/io/github/chubbyhippo/refresher/MessageServiceTest.java
@@ -1,13 +1,13 @@
 package io.github.chubbyhippo.refresher;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(classes = MessageService.class)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class MessageServiceTest {
 
     @Autowired
@@ -15,9 +15,19 @@ class MessageServiceTest {
 
     @Test
     @DisplayName("should return default message")
+    @Order(1)
     void shouldReturnDefaultMessage() {
         String result = messageService.getMessage();
         assertThat(result).isEqualTo("Default message");
+    }
+
+    @Test
+    @DisplayName("should return changed message")
+    @Order(2)
+    void shouldReturnChangedMessage() {
+        messageService.setMessage("Changed message");
+        String result = messageService.getMessage();
+        assertThat(result).isEqualTo("Changed message");
     }
 
 }

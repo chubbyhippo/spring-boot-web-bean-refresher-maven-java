38828438ff427503e63bd3df98d58cf18d4f0120
ch <mkmsacc@outlook.com>
2025-03-07 08:43:26 +0700

    Update Mockito imports in MessageControllerTest
    Removed unnecessary Mockito imports, retaining only 'when' for mocking behavior. This cleanup eliminates clutter and improves code maintainability without altering functionality.

--- src/test/java/io/github/chubbyhippo/refresher/MessageControllerTest.java
+++ src/test/java/io/github/chubbyhippo/refresher/MessageControllerTest.java
@@ -2,14 +2,12 @@ package io.github.chubbyhippo.refresher;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.assertj.MockMvcTester;
 
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.when;
 
 @WebMvcTest(MessageController.class)
 class MessageControllerTest {

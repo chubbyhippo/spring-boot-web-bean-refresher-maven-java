dbf6fd7491e79c2133c0b939f4f4cb6862cdbfcb
ch <mkmsacc@outlook.com>
2025-03-12 13:04:20 +0700

    Refactor and add test for setting custom message
    Removed unnecessary Mockito annotations and refactored the test to directly instantiate `MessageService`. Added a new test to verify behavior when a custom message is set.

--- src/test/java/io/github/chubbyhippo/refresher/MessageServiceTest.java
+++ src/test/java/io/github/chubbyhippo/refresher/MessageServiceTest.java
@@ -2,24 +2,25 @@ package io.github.chubbyhippo.refresher;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Spy;
-import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@ExtendWith(MockitoExtension.class)
 class MessageServiceTest {
-
-    @Spy
-    private MessageService messageService;
 
     @Test
     @DisplayName("should return default message")
     void shouldReturnDefaultMessage() {
-
-        String message = messageService.getMessage();
+        var messageService = new MessageService();
+        var message = messageService.getMessage();
         assertThat(message).isEqualTo("Default message");
+    }
+
+    @Test
+    @DisplayName("should return set message")
+    void shouldReturnSetMessage() {
+        var messageService = new MessageService();
+        messageService.setMessage("Set message");
+        assertThat(messageService.getMessage()).isEqualTo("Set message");
     }
 
 }

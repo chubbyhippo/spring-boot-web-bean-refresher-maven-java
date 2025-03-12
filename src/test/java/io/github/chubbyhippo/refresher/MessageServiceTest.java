7339c9155ad422d3b38b6f88e8f6a0f59c265522
ch <mkmsacc@outlook.com>
2025-03-12 13:01:55 +0700

    Refactor MessageServiceTest to use Mockito for testing
    Replaced @SpringBootTest with @ExtendWith(MockitoExtension) and switched to using @Spy for dependency injection. Removed unnecessary test methods and annotations to simplify and focus tests on core functionality. This improves test clarity and efficiency.

--- src/test/java/io/github/chubbyhippo/refresher/MessageServiceTest.java
+++ src/test/java/io/github/chubbyhippo/refresher/MessageServiceTest.java
@@ -1,33 +1,25 @@
 package io.github.chubbyhippo.refresher;
 
-import org.junit.jupiter.api.*;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest(classes = MessageService.class)
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@ExtendWith(MockitoExtension.class)
 class MessageServiceTest {
 
-    @Autowired
+    @Spy
     private MessageService messageService;
 
     @Test
     @DisplayName("should return default message")
-    @Order(1)
     void shouldReturnDefaultMessage() {
-        String result = messageService.getMessage();
-        assertThat(result).isEqualTo("Default message");
-    }
 
-    @Test
-    @DisplayName("should return changed message")
-    @Order(2)
-    void shouldReturnChangedMessage() {
-        messageService.setMessage("Changed message");
-        String result = messageService.getMessage();
-        assertThat(result).isEqualTo("Changed message");
+        String message = messageService.getMessage();
+        assertThat(message).isEqualTo("Default message");
     }
 
 }

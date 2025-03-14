6a668e17f2b5554a341fba0bbffe66dd25e8b530
chubbyhippo <mkmsacc@outlook.com>
2025-03-14 21:36:51 +0700

    "Fix inconsistent indentation in ClassControllerTest"
    Corrected inconsistent indentation within the ClassControllerTest file to improve readability and maintain coding standards. No functional or logic changes were made to the test.

--- src/test/java/io/github/chubbyhippo/refresher/clazz/ClassControllerTest.java
+++ src/test/java/io/github/chubbyhippo/refresher/clazz/ClassControllerTest.java
@@ -8,18 +8,18 @@ import org.springframework.test.web.servlet.assertj.MockMvcTester;
 
 @WebMvcTest(controllers = ClassController.class)
 public class ClassControllerTest {
-   @Autowired
-   private MockMvcTester mockMvcTester;
+    @Autowired
+    private MockMvcTester mockMvcTester;
 
     @Test
     @DisplayName("should return list of classes")
     void shouldReturnListOfClasses() {
-       mockMvcTester.get()
-               .uri("/classes")
-               .assertThat()
-               .hasStatusOk()
-               .bodyText()
-               .contains("ClassController");
+        mockMvcTester.get()
+                .uri("/classes")
+                .assertThat()
+                .hasStatusOk()
+                .bodyText()
+                .contains("ClassController");
 
     }
 }

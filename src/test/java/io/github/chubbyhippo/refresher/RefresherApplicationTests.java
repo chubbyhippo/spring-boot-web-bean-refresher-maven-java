55ab0cb676df50542f8ec46d0bf6857905cf26f7
chubbyhippo <mkmsacc@outlook.com>
2025-03-15 14:49:40 +0700

    Refactor test annotations and cleanup unnecessary tests
    Replaced `@DirtiesContext` strategy with `BEFORE_EACH_TEST_METHOD` for better test isolation. Removed redundant test methods to streamline the test suite. Simplified and retained the test for refresh scope validation.

--- src/test/java/io/github/chubbyhippo/refresher/RefresherApplicationTests.java
+++ src/test/java/io/github/chubbyhippo/refresher/RefresherApplicationTests.java
@@ -1,6 +1,7 @@
 package io.github.chubbyhippo.refresher;
 
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,48 +10,11 @@ import org.springframework.test.web.servlet.assertj.MockMvcTester;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 class RefresherApplicationTests {
 
     @Autowired
     private MockMvcTester mockMvcTester;
-
-    @Test
-    @DisplayName("should return message")
-    @Order(1)
-    void shouldReturnMessage() {
-        mockMvcTester.get()
-                .uri("/message")
-                .assertThat()
-                .hasBodyTextEqualTo("Default message");
-    }
-
-    @Test
-    @DisplayName("should return changed message")
-    @Order(2)
-    void shouldReturnChangedMessage() {
-        mockMvcTester.post()
-                .uri("/message")
-                .content("Changed message")
-                .assertThat()
-                .hasStatusOk();
-
-        mockMvcTester.get()
-                .uri("/message")
-                .assertThat()
-                .hasBodyTextEqualTo("Changed message");
-    }
-
-    @Test
-    @DisplayName("should return bean ahs been refreshed message")
-    @Order(3)
-    void shouldReturnBeanAhsBeenRefreshedMessage() {
-        mockMvcTester.post()
-                .uri("/refresh")
-                .assertThat()
-                .hasStatusOk();
-    }
 
     @Test
     @DisplayName("should trigger refresh scope")

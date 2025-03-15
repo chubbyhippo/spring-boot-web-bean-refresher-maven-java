76c273a9bd56b5a614f18fc8672e5d3f327490a5
chubbyhippo <mkmsacc@outlook.com>
2025-03-15 13:08:16 +0700

    Refactor BeanController to dynamically create beans
    Replaced hardcoded bean instantiation with dynamic creation using ClassService. This ensures flexibility by resolving the fully qualified class name at runtime and creating the corresponding bean instance. Updated the constructor and method signatures to support this change.

--- src/main/java/io/github/chubbyhippo/refresher/bean/BeanController.java
+++ src/main/java/io/github/chubbyhippo/refresher/bean/BeanController.java
@@ -1,6 +1,6 @@
 package io.github.chubbyhippo.refresher.bean;
 
-import io.github.chubbyhippo.refresher.message.MessageService;
+import io.github.chubbyhippo.refresher.clazz.ClassService;
 import org.springframework.beans.factory.support.DefaultSingletonBeanRegistry;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,16 +8,19 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
 @RestController
 public class BeanController {
     private final BeanService beanService;
     private final ConfigurableApplicationContext context;
+    private final ClassService classService;
 
-    public BeanController(BeanService beanService, ConfigurableApplicationContext context) {
+    public BeanController(BeanService beanService, ConfigurableApplicationContext context, ClassService classService) {
         this.beanService = beanService;
         this.context = context;
+        this.classService = classService;
     }
 
     @GetMapping("/beans")
@@ -26,9 +29,14 @@ public class BeanController {
     }
 
     @PostMapping("/beanRestart")
-    void restartBean(@RequestParam String beanName) {
+    void restartBean(@RequestParam String beanName) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException, ClassNotFoundException {
         DefaultSingletonBeanRegistry registry = (DefaultSingletonBeanRegistry) context.getBeanFactory();
         registry.destroySingleton(beanName);
-        registry.registerSingleton(beanName, new MessageService());
+
+        String fullyQualifiedNameBy = classService.getFullyQualifiedNameBy(beanName);
+        Class<?> clazz = Class.forName(fullyQualifiedNameBy);
+        Object beanInstance = clazz.getDeclaredConstructor().newInstance();
+
+        registry.registerSingleton(beanName, beanInstance);
     }
 }

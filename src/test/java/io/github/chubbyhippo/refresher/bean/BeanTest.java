85a1b8e6fc5634110d2b0c3af7dcead7d97d9834
chubbyhippo <mkmsacc@outlook.com>
2025-03-15 14:25:40 +0700

    Add integration tests for bean operations
    Introduce tests to verify the retrieval of bean names and the ability to restart a specific bean. These tests ensure proper functionality of endpoints and validate application behavior after bean restarts.

---
+++ src/test/java/io/github/chubbyhippo/refresher/bean/BeanTest.java
@@ -0,0 +1,67 @@
+package io.github.chubbyhippo.refresher.bean;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.web.servlet.assertj.MockMvcTester;
+
+import java.io.UnsupportedEncodingException;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+class BeanTest {
+
+    @Autowired
+    private MockMvcTester mockMvcTester;
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    @DisplayName("should return bean name list")
+    void shouldReturnBeanNameList() throws UnsupportedEncodingException, JsonProcessingException {
+        String contentAsString = mockMvcTester.get()
+                .uri("/beans")
+                .exchange()
+                .getResponse()
+                .getContentAsString();
+
+        List<String> beans = objectMapper.readValue(contentAsString, new TypeReference<>() {
+        });
+        assertThat(beans).isNotEmpty();
+        beans.forEach(System.out::println);
+    }
+
+    @Test
+    @DisplayName("should restart bean")
+    @DirtiesContext
+    void shouldRestartBean() {
+
+        mockMvcTester.post()
+                .uri("/message")
+                .content("Changed message")
+                .assertThat()
+                .hasStatusOk();
+
+        mockMvcTester.post()
+                .uri("/beanRestart")
+                .param("beanName", "messageService")
+                .assertThat()
+                .hasStatusOk();
+
+        mockMvcTester.get()
+                .uri("/message")
+                .assertThat()
+                .hasBodyTextEqualTo("Default message");
+
+    }
+}
